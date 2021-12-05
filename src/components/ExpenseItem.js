214c8ecd601938fc29af966cad7c869a1bc1d958
leshista <a.leshista@gmail.com>
2021-12-05 17:41:07 +0300

    Added a valid date presentation to component and learned how to work with date in general

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,27 @@
 import './ExpenseItem.css';
 
 export default function ExpenseItem(props) {
+    let day = props.date.toLocaleString('en-GB', {
+        day: 'numeric',
+        weekday: 'long',
+    });
+    let month = props.date.toLocaleString('en-GB', { month: 'long' });
+    let year = props.date.toLocaleString('en-GB', { year: 'numeric' });
+
+    let title = props.title;
+
+    let price = props.cost + ' ' + props.currency;
+
     return (
         <div className="expense-item">
-            <div>{props.date.toDateString()}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">
-                    {props.cost + ' ' + props.currency}
-                </div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">{price}</div>
             </div>
         </div>
     );
