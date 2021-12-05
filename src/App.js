6f1e8e9d3adafc36e6dcb8ef40951175f4ad0c51
leshista <a.leshista@gmail.com>
2021-12-05 13:21:23 +0300

    Learned how to create a custom component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
+import ExpenseItem from "./components/ExpenceItem";
+export default function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
-}
-
-export default App;
+};
